eec3b6d1f9a89e5386339f11fc1b043ae38fc445
Adrien-Mac <adrien@MacBook-Pro-de-Adrien-Mac.local>
2019-11-21 07:38:15 +0100

    init scala files

---
+++ src/main/scala/com/aguiot/placeholder/Main.scala
@@ -0,0 +1,7 @@
+package com.aguiot.placeholder
+
+object Main extends App {
+  def cube(x: Int): Int = {
+    x * x * x
+  }
+}
---
+++ src/test/scala/com/aguiot/placeholder/MainTest.scala
@@ -0,0 +1,9 @@
+package com.aguiot.placeholder
+
+import org.scalatest.FunSuite
+
+class MainTest extends FunSuite {
+  test("Main.cube") {
+    assert(Main.cube(3) === 27)
+  }
+}
